52325addeb2d4e18bb33c751b48164f291808df0
Hao Wu <hwu27@gmu.edu>
2025-12-25 22:57:14 -0500

    Consolidate ASAN docker scripts and make interactive by default
    - Change docker run to use -it and bash instead of -t and sleep infinity
    - Remove separate interactive script (functionality merged into main script)
    - Make install_triton.sh executable

--- setup_amd/start_docker_rocm_asan.sh
+++ setup_amd/start_docker_rocm_asan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -d -t \
+docker run -d -it \
     --name rocm_asan \
     --restart unless-stopped \
     --privileged \
@@ -12,4 +12,4 @@ docker run -d -t \
     -v "$PWD/..":/workspace \
     -w /workspace \
     pmylonamd/rocm7.0_ubuntu22.04_py3.10_pytorch_2.8.0_asan \
-    sleep infinity
+    bash
